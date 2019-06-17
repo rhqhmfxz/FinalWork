3fca0bdb0562510b6433b2fbb6158f5308997feb
Admin <Admin@Admin-PC.localdomain>
2019-06-17 15:55:05 +0800

    修复了登录功能出现的bug

--- src/dao/DataBaseConnection.java
+++ src/dao/DataBaseConnection.java
@@ -5,22 +5,22 @@ import java.sql.*;
 //实现数据库的连接与打开
 public class DataBaseConnection {
 	//定义数据库驱动类 
-    private final String DBDRIVER = "com.microsoft.sqlserver.jdbc.SQLServerDriverriver";
+    private final String DBDRIVER = "com.microsoft.sqlserver.jdbc.SQLServerDriver";
     //定义数据库连接URL  
     private final String DBURL = "jdbc:sqlserver://localhost:1433;Database=courseSystem";
     //定义数据库连接用户名
-    private final String DBUSER = "sa" ;  
+    private final String DBUSER = "sa";  
     //定义数据库连接密码  
-    private final String DBPASSWORD = "sa" ;  
+    private final String DBPASSWORD = "sa";  
     //定义数据库连接对象  
-    private Connection conn = null ;
+    private Connection conn = null;
     //构造方法，加载驱动 
     public DataBaseConnection(){  
         try{ 
         	//加载驱动
-            Class.forName(DBDRIVER) ; 
+            Class.forName(DBDRIVER); 
             //建立与数据库的连接
-            this.conn = DriverManager.getConnection(DBURL,DBUSER,DBPASSWORD) ;    
+            this.conn = DriverManager.getConnection(DBURL,DBUSER,DBPASSWORD);    
         }  
         catch (Exception e){ 
         	//若加载驱动失败,进行异常处理
